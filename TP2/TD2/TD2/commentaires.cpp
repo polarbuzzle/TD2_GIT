b4441e611858d803a96567cede3b950f4b4f5b04
J-F <jffontaine_@hotmail.com>
2017-02-03 09:23:05 -0500

    commentaires

---
+++ TP2/TD2/TD2/commentaires.cpp
@@ -0,0 +1,28 @@
+/**********************************************
+* Titre: Travail pratique #1 - Medecin.cpp
+* Date: 10 janvier 2017
+* Auteur: FERRON Samuel, FONTAINE Jean-Frederic
+*Description: Implémentation de la classe Medecin
+*********************************************/
+
+#include"Medecin.h"
+/*********************************************
+*Fonctions:		Constructeur par default
+*Descriptions:		Construit l'objet Medecin aves ses valeurs par defauts
+*Parametre:		Aucun
+*Retour:			Aucun
+*********************************************/
+Medecin::Medecin() {
+	nom_ = "Inconnu";
+	horaires_ = 0;
+	specialite_ = nullptr;
+}
+
+/*********************************************
+*Fonctions:		Medecin::Medecin
+*Descriptions:	Constructeur par défaut. Construit l'objet Specialite aves ses parametres
+*Parametre:		-(string)nom : nom du medecin (IN)
+-(int)horaires : nombres d'heures associé au medecin (IN)
+-(Specialite*) : Specilité du medecin (IN)
+*Retour:		Aucun
+*********************************************/
