cf12e7bcb655468ee5e12b3056a52145b5ce53d3
J-F <jffontaine_@hotmail.com>
2017-02-06 14:34:13 -0500

    Classe hopital

---
+++ TP2/TD2/TD2/Hopital.cpp
@@ -0,0 +1,77 @@
+/**********************************************
+* Titre: Travail pratique #1 - Medecin.cpp
+* Date: 10 janvier 2017
+* Auteur: FERRON Samuel, FONTAINE Jean-Frederic
+*Description: Implémentation de la classe Hopital 
+*********************************************/
+#include "Hopital.h"
+#include <string>
+#include <iostream>
+
+
+/*********************************************
+*Fonctions:		Hopital()
+*Descriptions:	Construit l'objet avec les parametres correspondant
+*Parametre:		-(string)nom	: Nom de lhopital 
+				-(Personnel)personnel	: Vecteur du personnel associe a cet hopital. 
+*Retour:			Aucun
+*********************************************/
+Hopital::Hopital(string nom, Personnel* personnel) : nom_(nom), personnel_(personnel) {
+
+}
+/*********************************************
+*Fonctions:		~Hopital()
+*Descriptions:	Destructeur de lobjet hopital 
+*Parametre:		Aucun 
+*Retour:		Aucun
+*********************************************/
+Hopital::~Hopital() {
+
+}
+
+/*********************************************
+*Fonctions:		accederNom()
+*Descriptions:	Acceseur du nom de lhopital 
+*Parametre:		Aucun
+*Retour:		-(string)nom_	: Nom de l'hopital 
+*********************************************/
+string Hopital::accederNom() {
+	return nom_; 
+}
+
+/*********************************************
+*Fonctions:		accederPersonnel()
+*Descriptions:	Acceseur du vecteur de Personnel 
+*Parametre:		Aucun
+*Retour:		-(Personnel)personnel_	: vecteur du personnel associe a lhopitla 
+*********************************************/
+Personnel Hopital::accederPersonnel() {
+	return *personnel_;
+}
+/*********************************************
+*Fonctions:		modifierNom()
+*Descriptions:	Permet de modifier le nom 
+*Parametre:		-(string)nom	: Nouveau nom qu'on veut donner
+*Retour:		-Aucun 
+*********************************************/
+void Hopital::modifierNom(string nom) {
+	nom_ = nom;
+}
+/*********************************************
+*Fonctions:		modifierPersonnel()
+*Descriptions:	Permet de modifier le vecteur de personnel 
+*Parametre:		-(Personnel*)personnel	: nouveau vecteur personnel
+*Retour:		-Aucun
+*********************************************/
+void Hopital::modifierPersonnel(Personnel* nouveau) {
+	personnel_ = nouveau; 
+}
+/*********************************************
+*Fonctions:		modifierPersonnel()
+*Descriptions:	Permet de modifier le vecteur de personnel
+*Parametre:		-(Personnel*)personnel	: nouveau vecteur personnel
+*Retour:		-Aucun
+*********************************************/
+ostream& operator<<(ostream& out, const Hopital& hopital) {
+	out<<hopital.accederPersonnel();
+}
--- TP2/TD2/TD2/Hopital.h
+++ TP2/TD2/TD2/Hopital.h
@@ -1,1 +1,30 @@
-#pragma once
+#ifndef HOPITAL_H
+#define HOPITAL_H
+#include "Personnel.h"
+#include <string>
+#include <iostream>
+
+using namespace std; 
+class Hopital
+{
+public: 
+	// Constructeur par parametres
+	Hopital(string nom, Personnel* personnel);
+	//Destructeur
+	~Hopital();
+	//Acceseur de lattribut nom
+	string accederNom();
+	//Acceseur de lattribut Personnel
+	Personnel accederPersonnel();
+	//Modificateur permettant de modifier le nom 
+	void modifierNom(string nom);
+	//Modificateur permettant de modifier le vecteur personnel
+	void modifierPersonnel(Personnel* nouveau);
+private:
+	//Attributs privees 
+	string nom_; 
+	Personnel* personnel_; 
+};
+//Surcharge d'operateur pour affichage 
+ostream& operator<<(ostream& out, const Hopital& hopital);
+#endif
